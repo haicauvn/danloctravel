1879875b3e3f7bc72078abea10e3e0cffebffe4c
nhathuu16021999 <49590630+nhathuu16021999@users.noreply.github.com>
2024-01-16 22:47:28 +0700

    Config firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+
+import 'firebase/analytics';
+import 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyC0wmZ2zUdYbVZgyACTWBBGZCKR2-O5QdE',
+  authDomain: 'danloctravel-8dc01.firebaseapp.com',
+  projectId: 'danloctravel-8dc01',
+  storageBucket: 'danloctravel-8dc01.appspot.com',
+  messagingSenderId: '929490657503',
+  appId: '1:929490657503:web:763d5d300c7d9636e7896a',
+  measurementId: 'G-FRPYQQ53HR',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebase.firestore();
+
+export { db };
+export default firebase;
