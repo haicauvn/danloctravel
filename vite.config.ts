b57770dc1e46eb6a570dc93223a30954b25a7f90
Hai Dang <haidv245@gmail.com>
2024-10-22 23:36:33 +0700

    Update viteTsconfigPaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import commonjs from "vite-plugin-commonjs"
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/danloctravel',
-  plugins: [react(), commonjs()],
+  plugins: [react(), commonjs(), viteTsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
