a1a194fd4aad153afa2bf89d93b356dfd17bd405
Hai Dang <haidv245@gmail.com>
2024-10-22 22:51:05 +0700

    Add base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import commonjs from "vite-plugin-commonjs"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/danloctravel',
   plugins: [react(), commonjs()],
   test: {
     globals: true,
